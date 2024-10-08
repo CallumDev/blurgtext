406726a592b599e61e29e4414a10fe2d3bf592dc
CallumDev <callum.mcging@gmail.com>
2024-10-08 21:00:40 +1030

    Win32: blurgtext.dll -> libblurgtext.dll. Fixes dotnet

--- dotnet/BlurgText/BlurgNative.cs
+++ dotnet/BlurgText/BlurgNative.cs
@@ -63,51 +63,51 @@ namespace BlurgText
         public delegate void blurg_texture_update(IntPtr texture, IntPtr buffer, int x, int y, int width,
             int height);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr blurg_create(IntPtr textureAllocate, IntPtr textureUpdate);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern int blurg_enable_system_fonts(IntPtr blurg);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr blurg_font_add_file(IntPtr blurg, IntPtr filename);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr blurg_font_query(IntPtr blurg, IntPtr familyName, int weight, int italic);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr blurg_font_get_family(IntPtr font);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern float blurg_font_get_line_height(IntPtr font, float size);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern int blurg_font_get_weight(IntPtr font);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern int blurg_font_get_italic(IntPtr font);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern void blurg_font_set_fallback(IntPtr font, IntPtr fallback);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void blurg_build_string_utf16(IntPtr blurg, IntPtr font, float size, uint color,
             IntPtr text, blurg_result_t *result);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void blurg_build_formatted(IntPtr formatted, IntPtr texts, int count,
             int measureCursor, float maxWidth, blurg_result_t* result);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void blurg_measure_string_utf16(IntPtr blurg, IntPtr font, float size, IntPtr text, float *width, float *height);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void blurg_measure_formatted(IntPtr blurg, IntPtr texts, int count, float maxWidth, float* width, float *height);
         
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern void blurg_free_result(IntPtr result);
 
-        [DllImport("blurgtext", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libblurgtext", CallingConvention = CallingConvention.Cdecl)]
         public static extern void blurg_destroy(IntPtr blurg);
     }
 }
